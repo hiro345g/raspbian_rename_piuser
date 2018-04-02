9f3877541e43ec1908ae123832e8fd2784077f24
hiro345g <hiro345@galileo.co.jp>
2018-04-02 14:55:08 +0900

    ユーザー名の変更はファイルを直接変更するので、リポジトリにはサンプルを登録するように修正

--- script/setup.sh
+++ script/setup.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+if [ ! -e rename_pi.sh ]; then
+  cp rename_pi_sample.sh rename_pi.sh
+fi
 sudo cp -a /etc/rc.local /etc/rc.local.backup
 sudo sed -i 's/exit 0//' /etc/rc.local
 cat rename_pi.sh | sudo tee -a /etc/rc.local
