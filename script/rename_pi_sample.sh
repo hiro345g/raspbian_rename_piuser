97f30cfd7cf43f139834d5d4f2b3cdcdc222524a
hiro345g <hiro345@galileo.co.jp>
2018-04-02 15:17:21 +0900

    pi001からpiに戻すときに、/home/piのシンボリックリンクは削除しておく必要があるので対応

--- script/rename_pi_sample.sh
+++ script/rename_pi_sample.sh
@@ -2,12 +2,12 @@ ouid="pi"
 nuid="pi001"
 /usr/sbin/usermod -l ${nuid} ${ouid}
 /usr/sbin/usermod -d /home/${nuid} -m ${nuid}
+[ -h /home/pi ] && /bin/rm /home/pi
 /usr/sbin/groupmod -n ${nuid} ${ouid}
 /bin/sed -E -i "s/^${ouid} /${nuid} /" /etc/sudoers.d/010_pi-nopasswd
 /bin/sed -E -i "s/--autologin ${ouid} /--autologin ${nuid} /" /etc/systemd/system/autologin@.service
 /bin/sed -E -i "s/autologin-user=${ouid}$/autologin-user=${nuid}/" /etc/lightdm/lightdm.conf
 /bin/sed -i "s/:${ouid};/:${nuid};/" /etc/polkit-1/localauthority.conf.d/60-desktop-policy.conf
-[ -h /home/pi ] && /bin/rm /home/pi
 [ ! -e /home/pi ] && /bin/ln -s /home/${nuid} /home/pi
 [ -f /var/spool/cron/crontabs/${ouid} ] && /bin/mv /var/spool/cron/crontabs/${ouid} /var/spool/cron/crontabs/${nuid}
 [ -f /var/spool/mail/${ouid} ] && /bin/mv /var/spool/mail/${ouid} /var/spool/mail/${nuid}
