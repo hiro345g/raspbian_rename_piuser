7455b29881c8a80c72e67a9906a2407a0054a0a9
hiro345g <hiro345@galileo.co.jp>
2018-04-02 15:33:03 +0900

    シンボリックリンクを削除するタイミングを間違えていたので修正

--- script/rename_pi_sample.sh
+++ script/rename_pi_sample.sh
@@ -1,8 +1,8 @@
 ouid="pi"
 nuid="pi001"
 /usr/sbin/usermod -l ${nuid} ${ouid}
-/usr/sbin/usermod -d /home/${nuid} -m ${nuid}
 [ -h /home/pi ] && /bin/rm /home/pi
+/usr/sbin/usermod -d /home/${nuid} -m ${nuid}
 /usr/sbin/groupmod -n ${nuid} ${ouid}
 /bin/sed -E -i "s/^${ouid} /${nuid} /" /etc/sudoers.d/010_pi-nopasswd
 /bin/sed -E -i "s/--autologin ${ouid} /--autologin ${nuid} /" /etc/systemd/system/autologin@.service
